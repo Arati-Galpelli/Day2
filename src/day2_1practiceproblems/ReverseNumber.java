8c451664679f92257ff24ade56aa834fcbff65f0
“AratiG“ <aratinalla@gmail.com>
2023-05-12 14:21:21 +0530

    Added: WhileLoopPrograms

---
+++ src/day2_1practiceproblems/ReverseNumber.java
@@ -0,0 +1,7 @@
+package day2_1practiceproblems;
+public class ReverseNumber {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/day2_1practiceproblems/SumOfNumbers.java
@@ -0,0 +1,6 @@
+package day2_1practiceproblems;
+public class SumOfNumbers {
+    public static void main(String[] args) {
+
+    }
+}
