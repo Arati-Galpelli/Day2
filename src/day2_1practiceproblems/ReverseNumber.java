58e835690a115defab6d38fc3c2e79c4aa18f27f
“AratiG“ <aratinalla@gmail.com>
2023-05-12 15:20:26 +0530

    Added: WhileLoopCode

--- src/day2_1practiceproblems/ReverseNumber.java
+++ src/day2_1practiceproblems/ReverseNumber.java
@@ -1,7 +1,22 @@
 package day2_1practiceproblems;
+
+import java.util.Scanner;
+
 public class ReverseNumber {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number which you want to reverse :");
+        int num = sc.nextInt();
+        reverse(num);
+    }
 
+    public static void reverse(int num) {
+        int rev = 0;
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num=num/10;
+        }
+        System.out.println("Reverse number is :" + rev);
     }
 
 }
--- src/day2_1practiceproblems/SumOfNumbers.java
+++ src/day2_1practiceproblems/SumOfNumbers.java
@@ -1,6 +1,21 @@
 package day2_1practiceproblems;
+
+import java.util.Scanner;
+
 public class SumOfNumbers {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number till which you want to sum :");
+        int num = sc.nextInt();
+        int res = sumOfNaturalNum(num);
+        System.out.println("Sum of"+" "+num+" "+"natural number is :"+res);
+    }
+    public static int sumOfNaturalNum(int n){
+        int i=0,sum=0;
+        while(i<=n){
+            sum = sum+i;
+            i++;
+        }
+        return sum;
     }
 }
