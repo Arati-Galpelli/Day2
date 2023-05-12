b0044701c62f261940fb8c80a97908e474651cf3
“AratiG“ <aratinalla@gmail.com>
2023-05-12 13:54:16 +0530

    Added : if-else_class

---
+++ src/day2practiceproblms/DigitToWord.java
@@ -0,0 +1,6 @@
+package day2practiceproblms;
+public class DigitToWord {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/day2practiceproblms/PlaceValue.java
@@ -0,0 +1,6 @@
+package day2practiceproblms;
+public class PlaceValue {
+    public static void main(String[] args){
+
+    }
+}
